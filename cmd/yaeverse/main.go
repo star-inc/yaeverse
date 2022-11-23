f71b51c32ef1191f29d410b7546039a8f52959df
SuperSonic <supersonic@livemail.tw>
2022-11-23 11:55:44 +0800

    init yaeverse

---
+++ cmd/yaeverse/main.go
@@ -0,0 +1,8 @@
+// Yae - The open-source hyper-metaverse based on instant messaging.
+// Copyright(c) 2022 Star Inc. All Rights Reserved.
+// The software licensed under Mozilla Public License Version 2.0
+
+package main
+
+func main() {
+}
